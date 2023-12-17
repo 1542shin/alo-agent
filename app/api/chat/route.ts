08d8332ab3719727ed9d72365a33f2ae3645b16f
1542shin <1542shin@gmail.com>
2023-12-17 09:59:15 -0500

    update prompt

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -11,7 +11,29 @@ const formatMessage = (message: VercelChatMessage) => {
   return `${message.role}: ${message.content}`;
 };
 
-const TEMPLATE = `You are a pirate named Patchy. All responses must be extremely verbose and in pirate dialect.
+const statement="My fellow citizens, crime rates have gone down since our administration took office. This is a direct result of our new tough-on-crime policies. Those who oppose our methods simply don't care about the safety of our nation. Remember, before we came into power, the opposition had decades to fix the problem, and they failed. It's either our proven approach or a return to the chaos of the past. We must not let the fearmongers and naysayers dictate our future. It's time to stay the course for a safer tomorrow."
+
+const fallacies=`1.The politician claims that the decrease in crime rates is a direct result of their policies, assuming a cause-and-effect relationship without providing concrete evidence that their policies are the cause of the decrease in crime.
+
+2. The statement presents the situation as having only two alternatives: the politician's approach or a return to chaos. This ignores other possible alternatives.
+
+3. The politician attacks the character of the opponents by suggesting they don't care about the safety of the nation, rather than addressing the opponents' arguments or policies.
+
+4. The politician uses fearmongering by suggesting that not following their policies would lead to chaos, playing on the audience's fears to win support.
+
+5. The politician may be misrepresenting the opposition's position by suggesting they had "decades to fix the problem" and did nothing, which may not accurately reflect the opposition's efforts or policies.
+
+6. Implicit in the statement is the idea that because their administration's policies are currently in place and crime has gone down, the policies must be correct and should be continued. This suggests that the popularity of the policy is an indicator of its correctness.
+`
+
+const TEMPLATE = `
+A user will be given a statement below from a politician. The statement has multiple logical fallacies. The user will evaluate if the statement is valid or not. Then you will evaluate if the user have found logical fallicies from the statement.
+
+#Here's the statement: 
+${statement}
+
+#The statement has following logical fallacies:
+${fallacies}
 
 Current conversation:
 {chat_history}
